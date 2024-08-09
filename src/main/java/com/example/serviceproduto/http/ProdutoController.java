3131637a771c53e3d391ae78f343a8b3de15e78b
Pamela Nakano <pamela.nakano@hotmail.com>
2024-08-09 17:55:56 -0300

    Atualização da documentação

--- src/main/java/com/example/serviceproduto/http/ProdutoController.java
+++ src/main/java/com/example/serviceproduto/http/ProdutoController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 public interface ProdutoController {
 
+    @Operation(summary = "Salva o produto de acordo com o JSON passado por parâmetro")
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     Produto save(@Valid @RequestBody ProdutoPersistDto dto);
@@ -36,16 +37,69 @@ public interface ProdutoController {
                     )
             )
     })
-
     @GetMapping("{id}")
     Produto findById(@PathVariable("id") Long id);
 
+    @Operation(summary = "Atualiza os atributos passados no body para o produto passado por parâmetro")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "404",
+                    content = @Content(
+                            mediaType = "application/json",
+                            examples = @ExampleObject(
+                                    value = """
+                                            {
+                                                "codigo": "X_100",
+                                                "mensagem": "Produto de código {id} não encontrado",
+                                                "documentacao": null
+                                            }
+                                            """
+                            )
+                    )
+            )
+    })
     @PatchMapping("{id}")
     Produto update(@PathVariable("id") Long id, @RequestBody JsonPatch patch);
 
+    @Operation(summary = "Deleta o produto passado por parâmetro")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "404",
+                    content = @Content(
+                            mediaType = "application/json",
+                            examples = @ExampleObject(
+                                    value = """
+                                            {
+                                                "codigo": "X_100",
+                                                "mensagem": "Produto de código {id} não encontrado",
+                                                "documentacao": null
+                                            }
+                                            """
+                            )
+                    )
+            )
+    })
     @DeleteMapping("{id}")
     void delete(@PathVariable("id") Long id);
 
+    @Operation(summary = "Atualiza todos os campos do produto passado por parâmetro, de acordo com o JSON fornecido")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "404",
+                    content = @Content(
+                            mediaType = "application/json",
+                            examples = @ExampleObject(
+                                    value = """
+                                            {
+                                                "codigo": "X_100",
+                                                "mensagem": "Produto de código {id} não encontrado",
+                                                "documentacao": null
+                                            }
+                                            """
+                            )
+                    )
+            )
+    })
     @PutMapping("{id}")
     Produto updateAll(@PathVariable("id") Long id, @RequestBody ProdutoPersistDto dto);
 
