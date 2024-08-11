7fe364c1a62db3bd633644e48d33173bd40e4a97
Pamela Nakano <pamela.nakano@hotmail.com>
2024-08-10 22:32:41 -0300

    Correções

--- src/main/java/com/example/serviceproduto/ServiceProdutoApplication.java
+++ src/main/java/com/example/serviceproduto/ServiceProdutoApplication.java
@@ -1,14 +1,8 @@
 package com.example.serviceproduto;
 
-import com.example.serviceproduto.model.Produto;
-import com.example.serviceproduto.repository.ProdutoRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-
-import java.math.BigDecimal;
 
 @SpringBootApplication
 @EnableDiscoveryClient
